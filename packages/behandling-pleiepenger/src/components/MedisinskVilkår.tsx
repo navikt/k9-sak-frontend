621aa5752d63b53cb2be0d1bc1ebcf4c78e502cf
Erlend Vige <erlend.edvardsen.vige@nav.no>
2020-11-26 11:35:57 +0100

    Add vurdering-mapping to MedisinskVilkår microfrontend

--- packages/behandling-pleiepenger/src/components/MedisinskVilkår.tsx
+++ packages/behandling-pleiepenger/src/components/MedisinskVilkår.tsx
@@ -1,20 +1,30 @@
 import * as React from 'react';
 import { MicroFrontend } from '@fpsak-frontend/utils';
+import { useHistory } from 'react-router-dom';
 
-const initializeMedisinskVilkår = elementId => {
-  (window as any).renderMedisinskVilkarApp(elementId);
+const initializeMedisinskVilkår = (elementId, history) => {
+  const urlParams = new URLSearchParams(`?${window.location.hash.substr(1)}`);
+  const vurdering = urlParams.get('vurdering');
+
+  (window as any).renderMedisinskVilkarApp(elementId, {
+    onSelectVurdering: (vurderingId) => {
+      history.push(`${window.location.search}#vurdering=${vurderingId}`);
+    },
+    vurdering
+  });
 };
 
 const medisinskVilkårAppID = 'medisinskVilkårApp';
 export default () => {
+  const history = useHistory();
   return (
     <MicroFrontend
       id={medisinskVilkårAppID}
-      jsSrc="/k9/microfrontend/medisinsk-vilkar/1.2.9/app.js"
+      jsSrc="http://localhost:8081/1.3.1/app.js"
       jsIntegrity="sha384-zTZOHy+icP1959vJmLfNltEiJ71a8PYaJFmG+uiUfCv3SET82YxEaeE2dHzAYiX3"
-      stylesheetSrc="/k9/microfrontend/medisinsk-vilkar/1.2.9/styles.css"
+      stylesheetSrc="http://localhost:8081/1.3.1/styles.css"
       stylesheetIntegrity="sha384-gG2QA/rYN+9l6GpiVzIZVspHGHD4c2vTW5ITdIUVBznVIGJqliuZfizW+Vk6Ltgq"
-      onReady={() => initializeMedisinskVilkår(medisinskVilkårAppID)}
+      onReady={() => initializeMedisinskVilkår(medisinskVilkårAppID, history)}
       onError={() => {}}
     />
   );
