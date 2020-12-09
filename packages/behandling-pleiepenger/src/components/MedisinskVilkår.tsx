7d61398d4585d992382a4303c0fdd0e5191dc984
Erlend Vige <erlend.edvardsen.vige@nav.no>
2020-12-09 14:25:33 +0100

    Bump medisinsk vilkår to 1.4.0

--- packages/behandling-pleiepenger/src/components/MedisinskVilkår.tsx
+++ packages/behandling-pleiepenger/src/components/MedisinskVilkår.tsx
@@ -2,7 +2,16 @@ import * as React from 'react';
 import { MicroFrontend } from '@fpsak-frontend/utils';
 
 const initializeMedisinskVilkår = elementId => {
-  (window as any).renderMedisinskVilkarApp(elementId);
+  (window as any).renderMedisinskVilkarApp(elementId, {
+    onVurderingValgt: vurdering => {
+      if (vurdering !== null) {
+        window.history.pushState('', '', `#vurdering=${vurdering}`);
+      } else {
+        window.location.hash = '';
+      }
+    },
+    vurdering: new URLSearchParams(`?${window.location.hash.substr(1)}`).get('vurdering'),
+  });
 };
 
 const medisinskVilkårAppID = 'medisinskVilkårApp';
@@ -10,10 +19,10 @@ export default () => {
   return (
     <MicroFrontend
       id={medisinskVilkårAppID}
-      jsSrc="/k9/microfrontend/medisinsk-vilkar/1.2.9/app.js"
-      jsIntegrity="sha384-zTZOHy+icP1959vJmLfNltEiJ71a8PYaJFmG+uiUfCv3SET82YxEaeE2dHzAYiX3"
-      stylesheetSrc="/k9/microfrontend/medisinsk-vilkar/1.2.9/styles.css"
-      stylesheetIntegrity="sha384-gG2QA/rYN+9l6GpiVzIZVspHGHD4c2vTW5ITdIUVBznVIGJqliuZfizW+Vk6Ltgq"
+      jsSrc="http://localhost:8081/1.4.0/app.js"
+      jsIntegrity="sha384-llEKrmj1uHpOpQYRZemaJ0NtcHivRMU7vo1KGyDXEcXqS/TKyrlpaFM8QUvkdg+p"
+      stylesheetSrc="http://localhost:8081/1.4.0/styles.css"
+      stylesheetIntegrity="sha384-x57YUQFT8ZhpI0Sma8G4bAQZuqT8ltiIJ4PN39WIa8nPVfhzGnWYzn9+PLZCBi7c"
       onReady={() => initializeMedisinskVilkår(medisinskVilkårAppID)}
       onError={() => {}}
     />
